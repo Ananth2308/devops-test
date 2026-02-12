672238e7a7179f7d0edca516cb52530c7160139a
Ananth2308 <138234986+Ananth2308@users.noreply.github.com>
2026-02-12 21:51:44 +0530

    Add unit test for infrastructure logic

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestInfrastructure(unittest.TestCase):
+    def test_logic(self):
+        # This is the test the AWS Agent will monitor
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
